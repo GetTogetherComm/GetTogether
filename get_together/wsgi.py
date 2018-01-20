5e97740f7c2fb748802a12308ef0ec0176caea98
Michael Hall <mhall119@gmail.com>
2018-01-20 16:52:19 -0500

    Include base project directory in python path for wsgi.py

--- get_together/wsgi.py
+++ get_together/wsgi.py
@@ -8,8 +8,10 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "get_together.settings")
 
