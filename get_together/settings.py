927933f68e1707ef580fa54ac6f5aae79c88c9f5
Michael Hall <mhall119@gmail.com>
2018-01-20 16:51:20 -0500

    Move loading local_settings overrides to the end of settings.py

--- get_together/settings.py
+++ get_together/settings.py
@@ -11,11 +11,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-try:
-    from local_settings import *
-
-except:
-    print("WARNING: You should create a local_settings.py to store local and secret data.")
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -149,4 +144,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from local_settings import *
+
+except:
+    print("WARNING: You should create a local_settings.py to store local and secret data.")
 
