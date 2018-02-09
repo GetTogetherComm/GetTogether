8c3358a60bf6e39b2ea4d697136ccff7410ca8da
Michael Hall <mhall119@gmail.com>
2018-02-09 15:35:40 -0500

    Show badge images in Admin interface

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import Account, Badge, BadgeGrant
 
@@ -8,7 +9,10 @@ class AccountAdmin(admin.ModelAdmin):
 admin.site.register(Account, AccountAdmin)
 
 class BadgeAdmin(admin.ModelAdmin):
-    list_display = ('name', 'img_url')
+    list_display = ('name', 'icon', 'img_url')
+    def icon(self, obj):
+        return (mark_safe('<img src="%s" title="%s" height="32px" width="32px"/>' % (obj.img_url, obj.name)))
+    icon.short_description = 'Icon'
 admin.site.register(Badge, BadgeAdmin)
 
 class GrantAdmin(admin.ModelAdmin):
