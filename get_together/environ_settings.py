dcdc250d6e3c8739f251d60eee1e19d3bbb3fd3a
Michael Hall <mhall119@gmail.com>
2018-12-24 13:35:18 -0500

    Cleanup Docker build and  instructions. Fixes #117

--- get_together/environ_settings.py
+++ get_together/environ_settings.py
@@ -15,4 +15,33 @@ DATABASES['default'].update(dj_database_url.config())
 MEDIA_URL = os.environ.get('MEDIA_URL', '/media/')
 STATIC_URL = os.environ.get('STATIC_URL', '/static/')
 
-EVENTS_TEAMS_DEFAULT_PREMIUM = os.environ.get('EVENTS_TEAMS_DEFAULT_PREMIUM', False)
+EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND', 'django.core.mail.backends.console.EmailBackend')
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', True)
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'you.smtp.host.com')
+EMAIL_PORT = os.environ.get('EMAIL_PORT', 587)
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', None)
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', None)
+
+SOCIAL_AUTH_GITHUB_KEY = os.environ.get('SOCIAL_AUTH_GITHUB_KEY', None)
+SOCIAL_AUTH_GITHUB_SECRET = os.environ.get('SOCIAL_AUTH_GITHUB_SECRET', None)
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.environ.get('SOCIAL_AUTH_GOOGLE_OAUTH2_KEY', None)
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.environ.get('SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET', None)
+
+SOCIAL_AUTH_FACEBOOK_KEY = os.environ.get('SOCIAL_AUTH_FACEBOOK_KEY', None)
+SOCIAL_AUTH_FACEBOOK_SECRET = os.environ.get('SOCIAL_AUTH_FACEBOOK_SECRET', None)
+
+SOCIAL_AUTH_TWITTER_KEY = os.environ.get('SOCIAL_AUTH_TWITTER_KEY', None)
+SOCIAL_AUTH_TWITTER_SECRET = os.environ.get('SOCIAL_AUTH_TWITTER_SECRET', None)
+
+SOCIAL_AUTH_LINKEDIN_KEY = os.environ.get('SOCIAL_AUTH_LINKEDIN_KEY', None)
+SOCIAL_AUTH_LINKEDIN_SECRET = os.environ.get('SOCIAL_AUTH_LINKEDIN_SECRET', None)
+
+# Needed to embed Google maps for event location
+GOOGLE_MAPS_API_KEY = os.environ.get('GOOGLE_MAPS_API_KEY', None)
+
+# If set, will include Google Analytics javascript in pages
+GOOGLE_ANALYTICS_ID = os.environ.get('GOOGLE_ANALYTICS_ID', None)
+
+# Free Geoip lookup from ipstack.com still requires an access token
+IPSTACK_ACCESS_KEY = os.environ.get('IPSTACK_ACCESS_KEY', None)
